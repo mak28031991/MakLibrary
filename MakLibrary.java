0fa8d307159389b0c385352a42de9d3795c463e7
mak28031991 <mak28031991@gmail.com>
2015-09-09 23:11:06 +0530

    Update MakLibrary.java

--- MakLibrary.java
+++ MakLibrary.java
@@ -9,7 +9,68 @@ public class MakLibrary {
 	    else
 	        return false;
 	}
-
+	public static int NextPowerOf2(int num)
+	{
+		num= num|num>>1;
+		num= num|num>>2;
+		num= num|num>>4;
+		num= num|num>>8;
+		num= num|num>>16;
+		
+		
+		return num+1;
+		
+	}
+	public static int PrevPowerOf2(int num)
+	{
+		num= num|num>>1;
+		num= num|num>>2;
+		num= num|num>>4;
+		num= num|num>>8;
+		num= num|num>>16;
+		
+		
+		return (num+1)>>1;
+		
+	}
+	
+	
+	public static int NoOfSetBits(int num)
+	{
+		int count=0;
+		while(num!=0)
+		{
+			if((num & 1) !=0)
+			{
+				count++;
+			}
+			num=num>>1;
+		}
+		return count;
+	}
+	public static boolean IsMultipleOf3(int num)
+	{
+		int odd_count=0;
+		int even_count=0;
+		if(num<0) num=-1*num;
+		if(num==0) return true;
+		if(num==1 || num==2) return false; 
+		while(num!=0)
+		{
+			if((num&1)!=0)
+			{
+				odd_count++;
+			}
+			num= num>>1;
+			if((num&1)!=0)
+			{
+				even_count++;
+			}
+			num= num>>1;
+		}
+		if(MakLibrary.IsMultipleOf3(odd_count-even_count)) return true;
+		else return false;
+	}
 	static boolean IsPowerOfTwo(int num)
 	{
 		return (num!=0 && (num & (num - 1))==0);	
@@ -31,23 +92,10 @@ public class MakLibrary {
 			System.out.println();
 		}
 	}
-	static long LargestPowerOfTwo(long N)
-	{
-	    //changing all left side bits to 1.
-	    N = N| (N>>1);
-	    N = N| (N>>2);
-	    N = N| (N>>4);
-	    N = N| (N>>8);
 
-	//as now the number is 2 * x-1, where x is required answer, so adding 1 and dividing it by 2.
-	 
-	            return (N+1)>>1;
-
-	}
-
-	static int count_of_ones=0;
 	static int OnesInBinaryRep(int num)
 	{
+		int count_of_ones=0;
 		while(num!=0)
 		{
 			num=(num)&(num-1);
@@ -55,5 +103,7 @@ public class MakLibrary {
 		}
 		return count_of_ones;
 	}
+	
+	
 	  
 }
