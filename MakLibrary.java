065572a4f03e0e0e001cfa617053719241674d07
mak28031991 <mak28031991@gmail.com>
2015-10-05 12:49:30 +0530

    Update MakLibrary.java

--- MakLibrary.java
+++ MakLibrary.java
@@ -128,5 +128,53 @@ public class MakLibrary {
 	}
 	
 	
+	//import java.util.HashMap;
+public static boolean IsIsomorphic(String s, String t)
+	{
+		if(s==null || t==null)
+		{
+			return false;
+		}
+		if(s.length() !=t.length())
+		{
+			return false;
+		}
+		else if (s.length()==0)
+		{
+			return true;
+		}
+		HashMap<Character, Character> map = new HashMap<Character, Character>();
+		int i=0;
+		while(i<s.length())
+		{
+			System.out.println(i);
+			char c1= s.charAt(i);
+			char c2= t.charAt(i);
+			
+			if(map.containsKey(c1))
+			{
+				if(map.get(c1)!=c2)
+				{
+					return false;
+				}
+				
+			}
+			else
+			{
+				if(!map.containsValue(c2))
+				{
+					map.put(c1, c2);
+				}
+				else
+				{
+					return false;
+				}
+			}	
+			i++;
+		}
+		return true;
+	}
+
+	
 	  
 }
